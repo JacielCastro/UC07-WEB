779e4b062368d553a021a2abbd9e1dcf5397934f
JOSE JACIEL SOUZA CASTRO <jacielcastro100@gmail.com>
2026-04-07 21:29:30 -0300

    -- Atualização --

--- Lista_Compras/src/Funcoes.js
+++ Lista_Compras/src/Funcoes.js
@@ -9,19 +9,68 @@ function adicionar() {
     }else if (categoria === '') {
         alert('Selecione a categoria')
     }
-    if (produto !== '' || categoria !== '') {
-        let lista = document.getElementById(`${categoria}`)
+    
+    let lista = document.getElementById(`${categoria}`)
     let item = document.createElement('li')
-    item.innerHTML = produto
-    lista.appendChild(item)
-    }        
+    let botao = document.createElement('button')
+    botao.textContent = '❌'
+    item.innerHTML = produto;
+    item.appendChild(botao);
+    lista.appendChild(item);    
+    document.getElementById('produto').value='';    
+     
+    botao.addEventListener('click',() => {
+        item.remove()
+    })    
 }
 
-let buttonExcluir = document.createElement('button');
-buttonExcluir.innerText = '❌';
-buttonExcluir.margiLeft = '10px';
-buttonExcluir.onclick = function(){ 
-    item.remove();
+function salva_lista() {
+    let lista = document.querySelectorAll('ul')
+    let dados = {}
+
+    lista.forEach(lista => {
+        let = categoria = lista.id
+        let itens = []
+
+        lista.querySelectorAll('li').forEach(item =>{
+            itens.push({
+                texto: item.firstChild.textContent,
+                concluido: item.classList.contains('concluindo')
+            })
+        })
+        dados[categoria] =itens
+    })
+    localStorage.setItem('listaProdutos',JSON.stringify(dados))
 }
-item.appendChild(buttonExcluir);
-lista.appendChild(item);
+
+function carregarDados() {
+    let dados = JSON.parse(localStorage.getItem(listaProdutos))
+
+    if (!dados) return
+    
+    for (let categoria in dados){
+        let lista = document.getElementById(categoria)
+        dados[categoria].forEach(itemData => {
+            criarItem(itemData.texto, categoria, itemData.concluido)
+        })
+    } 
+}
+
+/*function adicionar() {    
+    let produto = document.getElementById('produto').value
+    let categoria = document.getElementById('Categoria').value
+
+    if (produto === '') {
+        alert('Preencha o espaço produto')
+        return
+    }
+
+    criarItem(produto, categoria, false)
+    document.getElementById('produto').value = ''
+    salvarDados()
+}
+*/
+
+
+
+  
